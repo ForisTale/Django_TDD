d4f8c81bf77a828bd761be45485388ce1c16befe
Foris <foris4@gmail.com>
2019-06-30 14:56:11 +0100

    duplication item validation is done

--- lists/tests/test_view.py
+++ lists/tests/test_view.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.utils.html import escape
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR,
+    ExistingListItemForm, ItemForm
+)
 
 
 class HomePageTest(TestCase):
@@ -87,7 +90,7 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context["form"], ItemForm)
+        self.assertIsInstance(response.context["form"], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
@@ -96,7 +99,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f"/lists/{list_.id}/")
-        self.assertIsInstance(response.context["form"], ItemForm)
+        self.assertIsInstance(response.context["form"], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 
@@ -131,6 +134,18 @@ class NewListTest(TestCase):
         self.client.post("/lists/new", data={"text": ""})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_duplicate_item_validation_errors_end_up_on_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text="test")
+        response = self.client.post(
+            f"/lists/{list1.id}/",
+            data={"text": "test"}
+        )
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, "lists/list.html")
+        self.assertEqual(Item.objects.count(), 1)
 
 
 class NewItemTest(TestCase):

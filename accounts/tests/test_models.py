661c27d316be4d3f5b32b7fbee50cade039fc991
Foris <foris4@gmail.com>
2019-07-04 14:47:00 +0100

    change email field to unique and make model Token, add simple navbar

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from accounts.models import Token
 
 User = get_user_model()
 
@@ -9,10 +10,12 @@ class UserModelTest(TestCase):
     @staticmethod
     def test_user_is_valid_with_email_only():
         user = User(email="a@b.com")
-        user.full_clean() # should not raise
+        user.full_clean()  # should not raise
 
-    # NEVER DO IT FOR NORMAL WEBSITE!!!
-    # I do it now only to follow book
-    def test_email_is_primary_key(self):
-        user = User(email="a@b.com")
-        self.assertEqual(user.pk, "a@b.com")
+
+class TokenModelTest(TestCase):
+
+    def test_links_user_with_auto_generated_uid(self):
+        token1 = Token.objects.create(email="a@b.com")
+        token2 = Token.objects.create(email="a@b.com")
+        self.assertNotEqual(token1.uid, token2.uid)

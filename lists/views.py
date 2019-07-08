d72afeb52dcf6ece7492dd0fccd4d5fab26cd3a3
Foris <foris4@gmail.com>
2019-07-08 18:36:53 +0100

    NewListForm is done with all tests passed

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from lists.models import List
-from lists.forms import ItemForm, ExistingListItemForm
+from lists.forms import ItemForm, ExistingListItemForm, NewListForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -25,12 +25,21 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List()
-        list_.owner = request.user
+        if request.user.is_authenticated:
+            list_.owner = request.user
         list_.save()
         form.save(for_list=list_)
-        return redirect(list_)
+        return redirect(str(list_.get_absolute_url()))
     else:
         return render(request, "lists/home.html", {"form": form})
+
+
+def new_list2(request):
+    form = NewListForm(data=request.POST)
+    if form.is_valid():
+        list_ = form.save(owner=request.user)
+        return redirect(str(list_))
+    return render(request, "lists/home.html", {"form": form})
 
 
 def my_lists(request, email):
